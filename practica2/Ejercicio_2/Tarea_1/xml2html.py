c7e641ca093ccb0d482fb9874caaf57ae10a0a4a
lumialfe <uo270139@uniovi.es>
2020-10-26 18:44:40 +0100

    Ejercicio 1 Acabado. 2.1 Empezado

---
+++ practica2/Ejercicio_2/Tarea_1/xml2html.py
@@ -0,0 +1,39 @@
+"""
+Procesamiento genérico de archivos XML
+"""
+import xml.etree.ElementTree as ET
+
+def verXML(archivoXML):
+    try:
+        arbol = ET.parse(archivoXML)
+    except IOError:
+        print ('No se encuentra el archivo ', archivoXML)
+        exit()
+    except ET.ParseError:
+        print("Error procesando en el archivo XML = ", archivoXML)
+        exit()
+
+    raiz = arbol.getroot()
+    print("\nElemento raiz = ", raiz.tag)
+    if raiz.text != None:
+        print("Contenido = " , raiz.text.strip('\n')) #strip() elimina los '\n' del string
+    else:
+        print("Contenido = " , raiz.text)
+    print("Atributos = " , raiz.attrib)
+
+    # Recorrido de los elementos del árbol
+    for hijo in raiz.findall('.//'): # Expresión Path
+        print("\nElemento = " , hijo.tag)
+        if hijo.text != None:
+            print("Contenido = ", hijo.text.strip('\n')) #strip() elimina los '\n' del string
+        else:
+            print("Contenido = ", hijo.text)
+        print("Atributos = ", hijo.attrib)
+
+def main():
+    """Prueba de la función verXML()"""
+    print(verXML.__doc__)
+    miArchivoXML = input('Introduzca un archivo XML = ')
+    verXML(miArchivoXML)
+if __name__ == "__main__":
+    main()

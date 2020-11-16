265530a098407bc3a61ed002f30ee12f9e816f07
lumialfe <uo270139@uniovi.es>
2020-11-16 11:51:59 +0100

    Ej 7 wip

--- practica3/Ejercicio-7/Ejercicio7.js
+++ practica3/Ejercicio-7/Ejercicio7.js
@@ -1,9 +1,37 @@
 
-$(document).ready(function () {
-    $("#hide").click(function () {
+class Operator {
+    constructor() {
+
+    }
+
+    hide() {
         $("table").hide();
-    });
-    $("#show").click(function () {
+    }
+
+    show() {
         $("table").show();
-    });
-});
+    }
+
+    filter() {
+        var value = $("#text-filter").val().toLowerCase();
+        $("table tr").filter(function () {
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
+    }
+
+    addComment() {
+        var name = $("#name").val();
+        var comment = $("#comment").val()
+        var date = new Date();
+        var li = '<li id="ca"><p>'.concat(name).concat(": ").concat(comment).concat(" [").concat(date.toString()).concat('] </p><input type="button" id="removeComment" value="Remove Comment" />').concat("</li>");
+        $("#commentSection").append($(li));
+        $("#name").val("");
+        $("#comment").val("");
+
+        $("#removeComment").click(function () {
+            $("#ca").remove();
+        })
+    }
+}
+
+var o = new Operator();

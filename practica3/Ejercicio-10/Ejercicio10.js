0eab7c7387eae0832bc51e1c7c0bbfa25f36ae56
lumialfe <uo270139@uniovi.es>
2020-11-20 21:18:06 +0100

    Ex 10

---
+++ practica3/Ejercicio-10/Ejercicio10.js
@@ -0,0 +1,46 @@
+class ImageReaper {
+    constructor(keyWord) {
+        this.search = keyWord;
+    }
+
+    load() {
+        var word = this.search;
+        var flickrAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+        $.getJSON(flickrAPI,
+            {
+                tags: this.search,
+                tagmode: "any",
+                format: "json",
+                min_upload_date: "1990-01-01 00:00:00",
+                sort: "relevance",
+                content_type: "1"
+            })
+
+
+            .done(function (data) {
+
+                const myNode = document.getElementById("images");
+                while (myNode.firstChild) {
+                    myNode.removeChild(myNode.lastChild);
+                }
+
+                $('#images').append('<p>SHOWING REAPS FROM <a href="http://flicker.com" target="_blank">FLICKR</a> FOR KEYWORD <b>"' + word.toUpperCase() + '"</b>: </p><br/>');
+
+
+                $.each(data.items, function (i, item) {
+                    $("<img>").attr("src", item.media.m).appendTo("#images");
+                    if (i === 20) {
+                        return false;
+                    }
+                });
+            });
+    }
+
+    newSearch() {
+        this.search = document.getElementById('inputText').value;
+        this.load();
+    }
+
+}
+
+var imageReaper = new ImageReaper("Golf G60");

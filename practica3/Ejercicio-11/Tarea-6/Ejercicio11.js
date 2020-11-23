f0d31c3764f355fa1abf16a5532400b218942c10
lumialfe <uo270139@uniovi.es>
2020-11-23 14:28:07 +0100

    ex11 done

--- practica3/Ejercicio-11/Tarea-6/Ejercicio11.js
+++ practica3/Ejercicio-11/Tarea-6/Ejercicio11.js
@@ -1,49 +1,71 @@
 "use strict";
-class DyanamicMap {
+class MapGame {
     constructor() {
         this.randomLat = 0;
         this.randomLon = 0;
     }
-    initMap() {
-        var centro = { lat: 43.3672702, lng: -5.8502461 };
-        var mapaGeoposicionado = new google.maps.Map(document.getElementById('map'), {
-            zoom: 8,
-            center: centro,
-            mapTypeId: google.maps.MapTypeId.SATELLITE
-        });
 
-        var infoWindow = new google.maps.InfoWindow;
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(function (position) {
+    guess() {
+        var inputLat = document.getElementById('inLat').value;
+        var inputLon = document.getElementById('inLon').value;
 
-                var lat = 0;
-                var lon = 0;
-                lat = parseFloat(Math.random() * (180) - 90);
-                lat = Math.round(lat * 10000) / 10000
-                lon = parseFloat(Math.random() * (180) - 90);
-                lon = Math.round(lat * 10000) / 10000
-                
-                var pos = new google.maps.LatLng({lat, lon});
-
-                infoWindow.setPosition(pos);
-                infoWindow.setContent('Localización encontrada');
-                infoWindow.open(mapaGeoposicionado);
-                mapaGeoposicionado.setCenter(pos);
-            }, function () {
-                handleLocationError(true, infoWindow, mapaGeoposicionado.getCenter());
-            });
+        if (inputLat=="" || inputLon=="") {
+            alert("Please enter valid coordinates");
         } else {
-            // Browser doesn't support Geolocation
-            handleLocationError(false, infoWindow, mapaGeoposicionado.getCenter());
+            var cell = document.getElementById("gaem");
+
+            if (cell.hasChildNodes()) {
+                while (cell.childNodes.length >= 1) {
+                    cell.removeChild(cell.firstChild);
+                }
+            }
+    
+            var ubicacion = document.getElementById('gaem');
+            ubicacion.innerHTML = '<p><b>RESULTADO: </b></p><p>Tu seleccion: Latitud: ' + inputLat + ', Longitud: ' + inputLon + '.</p><p>Solución: Latitud: ' + this.randomLat + ', Longitud: ' + this.randomLon + '.</p>';
         }
+        
     }
-    handleLocationError(browserHasGeolocation, infoWindow, pos) {
-        infoWindow.setPosition(pos);
-        infoWindow.setContent(browserHasGeolocation ?
-            'Error: Ha fallado la geolocalización' :
-            'Error: Su navegador no soporta geolocalización');
-        infoWindow.open(mapaGeoposicionado);
+
+    showMap() {
+        var ubicacion = document.getElementById('map');
+
+        var latR = 0;
+        var lngR = 0;
+        latR = parseFloat(Math.random() * (43.4054 - 42.4640) + 42.4650);
+        latR = parseFloat(Math.round(latR * 10000) / 10000);
+        lngR = parseFloat(Math.random() * (-4.3048 + 7.0305) - 7.0305);
+        lngR = parseFloat(Math.round(lngR * 10000) / 10000);
+
+        this.randomLat = latR;
+        this.randomLon = lngR;
+
+        var apiKey = "&key=AIzaSyC6j4mF6blrc4kZ54S6vYZ2_FpMY9VzyRU";
+        //URL: obligatoriamente https
+        var url = "https://maps.googleapis.com/maps/api/staticmap?";
+        //Parámetros
+        // centro del mapa (obligatorio si no hay marcadores)
+        var centro = "center=" + latR + "," + lngR;
+        //zoom (obligatorio si no hay marcadores)
+        //zoom: 1 (el mundo), 5 (continentes), 10 (ciudad), 15 (calles), 20 (edificios)
+        var zoom = "&zoom=10";
+        //Tamaño del mapa en pixeles (obligatorio)
+        var tamaño = "&size=1000x400";
+        //Escala (opcional)
+        //Formato (opcional): PNG,JPEG,GIF
+        //Tipo de mapa (opcional)
+        var mapType = "&mapType=satellite"
+        //Idioma (opcional)
+        //region (opcional)
+        //marcadores (opcional)
+        var marcador = "&markers=color:red%7Clabel:%7C" + latR + "," + lngR;
+        //rutas. path (opcional)
+        //visible (optional)
+        //style (opcional)
+        var sensor = "&sensor=false";
+
+        this.imagenMapa = url + centro + zoom + tamaño + marcador + mapType + sensor + apiKey;
+        ubicacion.innerHTML = "<img src='" + this.imagenMapa + "'/>";
     }
 }
 
-var geo = new DyanamicMap();
+var geo = new MapGame();

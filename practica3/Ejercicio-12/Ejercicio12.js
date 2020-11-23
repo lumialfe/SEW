3c6aa0ec80c121d982e780a2fff2970f0abf4c6d
lumialfe <uo270139@uniovi.es>
2020-11-23 15:00:27 +0100

    awertghj

---
+++ practica3/Ejercicio-12/Ejercicio12.js
@@ -0,0 +1,40 @@
+class FileLoader {
+    contructor() {
+
+    }
+
+    load(files) {
+        //Solamente toma un archivo
+        //var archivo = document.getElementById("archivoTexto").files[0];
+        var archivo = files[0];
+        var nombre = document.getElementById("nombreArchivo");
+        var tamaño = document.getElementById("tamañoArchivo");
+        var tipo = document.getElementById("tipoArchivo");
+        var ultima = document.getElementById("ultimaModificacion");
+        var contenido = document.getElementById("contenidoArchivo");
+        var areaVisualizacion = document.getElementById("areaTexto");
+        var errorArchivo = document.getElementById("errorLectura");
+        nombre.innerText = "Nombre del archivo: " + archivo.name;
+        tamaño.innerText = "Tamaño del archivo: " + archivo.size + " bytes";
+        tipo.innerText = "Tipo del archivo: " + archivo.type;
+        ultima.innerText = "Fecha de la última modificación: " + archivo.lastModifiedDate;
+        contenido.innerText = "Contenido del archivo de texto:"
+
+        //Solamente admite archivos de tipo texto
+        if ((archivo.type.match("text/plain")) || (archivo.type.match("application/json")) || (archivo.type.match("text/xml"))) {
+            var lector = new FileReader();
+            lector.onload = function (evento) {
+                //El evento "onload" se lleva a cabo cada vez que se completa con éxito una operación de lectura
+                //La propiedad "result" es donde se almacena el contenido del archivo
+                //Esta propiedad solamente es válida cuando se termina la operación de lectura
+                areaVisualizacion.innerText = lector.result;
+            }
+            lector.readAsText(archivo);
+        } else {
+            errorArchivo.innerText = "Error : Formato de archivo no válido";
+        }
+        
+    }
+}
+
+var fl = new FileLoader();

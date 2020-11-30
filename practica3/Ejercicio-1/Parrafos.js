8a3ad96813350ba50719fdf90e138f062a0eb958
lumialfe <uo270139@uniovi.es>
2020-11-30 16:23:00 +0100

    1

--- practica3/Ejercicio-1/Parrafos.js
+++ practica3/Ejercicio-1/Parrafos.js
@@ -1,27 +1,27 @@
 // 73-Parrafos.js
 // Version 1.0. 29/10/2020. Luis Miguel Alonso Ferreiro (UO270139). Universidad de Oviedo
 
-document.write("<p><b>Informe de la asignatura:</b> ");
+document.write("<p><strong>Informe de la asignatura:</strong> ");
 document.write(asignatura.nombre);
 document.write("</p>");
-document.write("<p><b>Titulación:</b> ");
+document.write("<p><strong>Titulación:</strong> ");
 document.write(asignatura.titulacion);
 document.write("</p>");
-document.write("<p><b>Centro:</b> ");
+document.write("<p><strong>Centro:</strong> ");
 document.write(asignatura.centro);
 document.write("</p>");
-document.write("<p><b>Universidad:</b> ");
+document.write("<p><strong>Universidad:</strong> ");
 document.write(asignatura.universidad);
 document.write("</p>");
-document.write("<p><b>Curso actual:</b> ");
+document.write("<p><strong>Curso actual:</strong> ");
 document.write(asignatura.curso);
 document.write("</p>");
 
 var i;
 for (i = 0; i < 3; i++) {
-    document.write("<p><b>Profesor:</b> ");
+    document.write("<p><strong>Profesor:</strong> ");
     document.write(asignatura.profesores[i]);
-    document.write(". <b>E-mail:</b> ");
+    document.write(". <strong>E-mail:</strong> ");
     document.write(asignatura.emails[i]);
     document.write("</p>");
 }

7733a9b8161342932d59f6658579b7194440f926
lumialfe <uo270139@uniovi.es>
2020-11-30 11:16:34 +0100

    d

--- practica3/Ejercicio-10/Ejercicio10.js
+++ practica3/Ejercicio-10/Ejercicio10.js
@@ -28,7 +28,9 @@ class ImageReaper {
 
 
                 $.each(data.items, function (i, item) {
-                    $("<img>").attr("src", item.media.m).appendTo("#images");
+                    // console.log(item.media);
+                    // $('#images').append('img src="' + item.media.m);
+                    $("<img>").attr("src", item.media.m).attr("alt", item.media.m).appendTo("#images");
                     if (i === 20) {
                         return false;
                     }
